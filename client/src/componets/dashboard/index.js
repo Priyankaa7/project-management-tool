f24eb77732dfee7a13e26429490f0a21a9f0d07a
Aayush Yadav <yadavaayush002@gmail.com>
2023-07-05 14:00:29 +0530

    fixed homepage

--- client/src/componets/dashboard/index.js
+++ client/src/componets/dashboard/index.js
@@ -38,34 +38,38 @@ function Dashboard(props) {
       }
 
       return (
-        <div className="h-screen overflow-y-scroll">
+        <div>
           {!authenticated && <><Hero /></>}
-          {dashboardData?.projectData && dashboardData.projectData.length > 0 ? (
-            <>
-              <div className="bg-black md:my-5 md:py-10 p-5 space-y-10 md:px-40 md:mx-10 flex flex-col md:flex-row md:justify-between items-center">
-                <h1 className="w-fit text-7xl text-primary_text font-bold underline underline-offset-8 decoration-brand">Projects</h1>
-                <div className="w-fit">
-                  <input className="bg-black border-[1px] border-white/30 outline-none p-2 rounded w-80 text-sm text-secondary" type="search" placeholder="Search Projects..." />
-                </div>
-              </div>
-              <div className="lg:grid lg:grid-cols-4 md:grid-cols-3 grid grid-cols-1 ">
-                {dashboardData.projectData.map(singleProject => (
-                  <div className="p-5 md:p-0 text-primary_text" key={singleProject.projectId} onClick={() => { navigate(`/project?projectId=${singleProject.projectId}`) }}>
-                    <div className={` ${getRandomColorClass()} cursor-pointer to-black bg-gradient-to-b  py-4 px-5 border-2 border-white/10 rounded-2xl flex flex-col items-center justify-center text-center space-y-4`}>
-                      <h2 className="capitalize text-3xl">
-                        {singleProject.projectTitle}
-                      </h2>
-                      <p className="text-sm text-secondary">{singleProject.projectDescription}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </>
-          ) : (
-            <div className="flex items-center justify-center h-screen bg-black">
-              <p className="text-2xl text-primary_text underline underline-offset-8 decoration-brand underline-2">You do not have any projects.</p>
+          {authenticated && (
+  dashboardData?.projectData && dashboardData.projectData.length > 0 ? (
+    <div className="h-screen overflow-y-scroll">
+      <div className="bg-black md:my-5 md:py-10 p-5 space-y-10 md:px-40 md:mx-10 flex flex-col md:flex-row md:justify-between items-center">
+        <h1 className="w-fit text-7xl text-primary_text font-bold underline underline-offset-8 decoration-brand">Projects</h1>
+        <div className="w-fit">
+          <input className="bg-black border-[1px] border-white/30 outline-none p-2 rounded w-80 text-sm text-secondary" type="search" placeholder="Search Projects..." />
+        </div>
+      </div>
+      <div className="lg:grid lg:grid-cols-4 md:grid-cols-3 grid grid-cols-1">
+        {dashboardData.projectData.map(singleProject => (
+          <div className="p-5 md:p-0 text-primary_text" key={singleProject.projectId} onClick={() => { navigate(`/project?projectId=${singleProject.projectId}`) }}>
+            <div className={` ${getRandomColorClass()} cursor-pointer to-black bg-gradient-to-b  py-4 px-5 border-2 border-white/10 rounded-2xl flex flex-col items-center justify-center text-center space-y-4`}>
+              <h2 className="capitalize text-3xl">
+                {singleProject.projectTitle}
+              </h2>
+              <p className="text-sm text-secondary">{singleProject.projectDescription}</p>
             </div>
-          )}
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <p className="text-2xl text-primary_text underline underline-offset-8 decoration-brand underline-2">You do not have any projects.</p>
+    </div>
+  )
+)}
+
+          
         </div>
       );
 }
